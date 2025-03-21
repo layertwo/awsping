79620386e8423f6e0d1c0d929e8330b43d50e336
Lucas Messenger <1335960+layertwo@users.noreply.github.com>
2025-03-20 21:47:28 -0700

    chore: allow for getting region names from list

--- target.go
+++ target.go
@@ -19,19 +19,27 @@ type AWSTarget struct {
 	Rnd     string
 }
 
+func GetTld(code string) string {
+	tld := "amazonaws.com"
+	if code[:2] == "cn" {
+		tld = "amazonaws.com.cn"
+	}
+	return tld
+}
+
 // GetURL return URL for AWS target
 func (r *AWSTarget) GetURL() string {
 	proto := "http"
 	if r.HTTPS {
 		proto = "https"
 	}
-	hostname := fmt.Sprintf("%s.%s.amazonaws.com", r.Service, r.Code)
+	hostname := fmt.Sprintf("%s.%s.%s", r.Service, r.Code, GetTld(r.Code))
 	url := fmt.Sprintf("%s://%s/ping?x=%s", proto, hostname, r.Rnd)
 	return url
 }
 
 // GetIP return IP for AWS target
 func (r *AWSTarget) GetIP() (*net.TCPAddr, error) {
-	tcpURI := fmt.Sprintf("%s.%s.amazonaws.com:80", r.Service, r.Code)
+	tcpURI := fmt.Sprintf("%s.%s.%s:443", r.Service, r.Code, GetTld(r.Code))
 	return net.ResolveTCPAddr("tcp4", tcpURI)
 }
--- utils.go
+++ utils.go
@@ -1,9 +1,11 @@
 package awsping
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"math/rand"
+	"net/http"
 	"os"
 	"sort"
 	"strconv"
@@ -118,36 +120,38 @@ func (lo *LatencyOutput) Show(regions *AWSRegions) {
 
 // GetRegions returns a list of regions
 func GetRegions() AWSRegions {
-	return AWSRegions{
-		NewRegion("Africa (Cape Town)", "af-south-1"),
-		NewRegion("Asia Pacific (Hong Kong)", "ap-east-1"),
-		NewRegion("Asia Pacific (Tokyo)", "ap-northeast-1"),
-		NewRegion("Asia Pacific (Seoul)", "ap-northeast-2"),
-		NewRegion("Asia Pacific (Osaka)", "ap-northeast-3"),
-		NewRegion("Asia Pacific (Mumbai)", "ap-south-1"),
-		NewRegion("Asia Pacific (Hyderabad)", "ap-south-2"),
-		NewRegion("Asia Pacific (Singapore)", "ap-southeast-1"),
-		NewRegion("Asia Pacific (Sydney)", "ap-southeast-2"),
-		NewRegion("Asia Pacific (Jakarta)", "ap-southeast-3"),
-		NewRegion("Asia Pacific (Melbourne)", "ap-southeast-4"),
-		NewRegion("Canada (Central)", "ca-central-1"),
-		NewRegion("Europe (Frankfurt)", "eu-central-1"),
-		NewRegion("Europe (Zurich)", "eu-central-2"),
-		NewRegion("Europe (Stockholm)", "eu-north-1"),
-		NewRegion("Europe (Milan)", "eu-south-1"),
-		NewRegion("Europe (Spain)", "eu-south-2"),
-		NewRegion("Europe (Ireland)", "eu-west-1"),
-		NewRegion("Europe (London)", "eu-west-2"),
-		NewRegion("Europe (Paris)", "eu-west-3"),
-		NewRegion("Middle East (UAE)", "me-central-1"),
-		NewRegion("Middle East (Bahrain)", "me-south-1"),
-		NewRegion("South America (Sao Paulo)", "sa-east-1"),
-		NewRegion("US East (N. Virginia)", "us-east-1"),
-		NewRegion("US East (Ohio)", "us-east-2"),
-		NewRegion("US West (N. California)", "us-west-1"),
-		NewRegion("US West (Oregon)", "us-west-2"),
-		NewRegion("Israel (Tel Aviv)", "il-central-1"),
+	// URL of the region code to name mapping JSON file
+	url := "https://raw.githubusercontent.com/burib/aws-region-table-parser/refs/heads/master/region_code_to_name_map.json"
+
+	// Make HTTP GET request
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Errorf("failed to fetch regions JSON: %v", err)
 	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	// Decode JSON into a map
+	var regionMap map[string]string
+	if err := json.NewDecoder(resp.Body).Decode(&regionMap); err != nil {
+		fmt.Errorf("failed to decode JSON: %v", err)
+	}
+
+	// Convert map to slice of Region structs
+	regions := make([]AWSRegion, 0, len(regionMap))
+	for code, name := range regionMap {
+		regions = append(regions, NewRegion(name, code))
+	}
+
+	// Sort by region code for consistent output
+	sort.Slice(regions, func(i, j int) bool {
+		return regions[i].Code < regions[j].Code
+	})
+
+	return regions
 }
 
 // CalcLatency returns list of aws regions sorted by Latency
